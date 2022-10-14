3becfd9be2981eec9b5013baf5f008b642b266fd
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-10-14 16:41:49 +0300

    Init

---
+++ src/main/java/ua/kovalchuk/sheets/GoogleSheetsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.sheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSheetsIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleSheetsIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kovalchuk/sheets/GoogleSheetsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.sheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSheetsIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
